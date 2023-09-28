9192321b8ac1958b4abd634b709815341d27914a
JRoccaMonti <Rocca.Jano@gmail.com>
2023-09-28 14:46:41 -0300

    server

--- server/src/server.js
+++ server/src/server.js
@@ -2,16 +2,35 @@ const express = require("express");
 const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
-const path = require("path");
+const cookieParser = require('cookie-parser');
 
 const server = express();
 
 server.use(express.static(__dirname + '/public'));
+server.use(express.urlencoded({ extended: true }));
+
+server.use(cookieParser());
 
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
 server.use(cors());
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "https://pi-drivers-deploy.vercel.app/");
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 server.use('/',router);
+
+server.use((err, req, res, next) => { 
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
 
 module.exports = server;
