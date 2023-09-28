39c0a5aaade6a9730631a5f65d3af94af33a6d6d
JRoccaMonti <Rocca.Jano@gmail.com>
2023-09-28 15:36:35 -0300

    marianoooo

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "https://pi-drivers-deploy.vercel.app/");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
